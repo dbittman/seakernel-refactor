ece2ef403118d26d8084c7cc0c21adc26bf0527a
Daniel Bittman <danielbittman1@gmail.com>
2016-04-17 18:06:20 -0700

    Add vga device.

---
+++ machine/pc/devices/vga.c
@@ -0,0 +1,36 @@
+#include <device.h>
+#include <system.h>
+#include <file.h>
+#include <frame.h>
+#include <map.h>
+#include <fs/sys.h>
+static struct device dev;
+
+static bool _vga_map(struct file *file, struct mapping *map)
+{
+	(void)file;
+	frame_acquire(0xB8000);
+	map->frame = 0xB8000;
+	return true;
+}
+
+static struct file_calls vga_calls = {
+	.read = 0,
+	.write = 0,
+	.create = 0, .destroy = 0, .ioctl = 0, .select = 0, .open = 0, .close = 0,
+	.map = _vga_map, .unmap = 0,
+};
+
+static void _late_init(void)
+{
+	int ret = sys_mknod("/dev/vga", S_IFCHR | 0666, makedev(dev.devnr, 0));
+	assert(ret == 0);
+}
+
+__orderedinitializer(__orderedafter(DEVICE_INITIALIZER_ORDER))
+static void _serial_init(void)
+{
+	dev_register(&dev, &vga_calls, S_IFCHR);
+	init_register_late_call(&_late_init, NULL);
+}
+
